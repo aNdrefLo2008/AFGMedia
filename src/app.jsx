3766df1dddb5dc6048da602d1f4e3556a7fa10d3
Andrei Florea <floreaalexandru146@gmail.com>
2023-05-16 16:50:44 +0200

    Additional perfectioning

--- src/app.jsx
+++ src/app.jsx
@@ -3,17 +3,22 @@ import { BrowserRouter } from 'react-router-dom'
 
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components'
 
+import { Analytics } from '@vercel/analytics/react';
+
 const App = () => {
   return (
     <BrowserRouter>
         <div className='relative z-0 bg-primary'>
             <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+                <Analytics />
                 <Navbar />
                 <Hero />
             </div>
             <About />
             {/*<Experience /> */}
-            <Tech />
+            <div className='hidden sm:block'>
+              <Tech />
+            </div>
             <Works />
             {/* <Feedbacks /> */}
             <div className='relative z-0'>
